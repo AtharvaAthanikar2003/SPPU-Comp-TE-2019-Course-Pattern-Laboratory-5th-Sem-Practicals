2124bc5fba5c6bcabb9b73614d4a68fcfd761372
Atharva Athanikar <120152105+AtharvaAthanikar2003@users.noreply.github.com>
2023-09-18 23:11:01 +0530

    Update DBMS_05.sql

--- DBMSL/DBMS_05.sql
+++ DBMSL/DBMS_05.sql
@@ -1,4 +1,5 @@
 /*
+DBMS Exp No.5 :- Named PL/SQL Block: PL/SQL Stored Procedure and Stored Function.
 Problem Statement : Write a Stored Procedure namely proc_Grade for the categorization of student. 
 If marks scored by students in examination is <=1500 and marks>=990 then student will be placed in distinction category, 
 if marks scored are between 989 and900 category is first class, if marks 899 and 825 category is Higher Second Class.
