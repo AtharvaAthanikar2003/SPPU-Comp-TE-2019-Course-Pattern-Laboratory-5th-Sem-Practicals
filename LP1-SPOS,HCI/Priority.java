c5cf5e48fab040a25dc46a08d0a6c09567555744
Atharva Athanikar <120152105+AtharvaAthanikar2003@users.noreply.github.com>
2023-11-01 23:21:41 +0530

    Update Priority.java

--- LP1-SPOS,HCI/Priority.java
+++ LP1-SPOS,HCI/Priority.java
@@ -13,19 +13,19 @@ public class Priority {
             int pt[] = new int[n];
             int wt[] = new int[n];
             int tat[] = new int[n];
-            int ct[] = new int[n];
+            int ct[] = new int[n];            
             for (i = 0; i < n; i++) {
                 p[i] = i + 1;
                 bt[i] = sc.nextInt();
-            }
+            }         
             System.out.println("Enter arrival times: ");
             for (i = 0; i < n; i++) {
                 at[i] = sc.nextInt();
-            }
+            }            
             System.out.println("Enter priority times: ");
             for (i = 0; i < n; i++) {
                 pt[i] = sc.nextInt();
-            }
+            }           
             for (i = 0; i < n; i++) {
                 pos = i;
                 for (int j = i + 1; j < n; j++) {
@@ -45,25 +45,31 @@ public class Priority {
                 temp = at[pos];
                 at[pos] = at[i];
                 at[i] = temp;
-            }
+            }           
             wt[0] = 0;
             for (i = 1; i < n; i++) {
-                wt[i] = wt[i - 1] + bt[i - 1];
-            }
+                wt[i] = ct[i - 1] + bt[i - 1];
+            }           
             System.out.print("-------------------------------------------------------------------------------------------------------------------");
             System.out.print("\nProcess\t\tArrival Time\tBurst Time\tCompletion Time\t\tTurnaround Time\t\tWaiting Time\n");
-            System.out.print("-------------------------------------------------------------------------------------------------------------------");
+            System.out.print("-------------------------------------------------------------------------------------------------------------------");            
             for (i = 0; i < n; i++) {
                 ct[i] = at[i] + wt[i] + bt[i];
                 tat[i] = ct[i] - at[i];
-                wt[i] = tat[i] - bt[i];
                 totalTime += tat[i];
                 System.out.println("\n " + p[i] + "\t\t" + at[i] + "\t\t" + bt[i] + "\t\t" + ct[i] + "\t\t\t" + tat[i] + "\t\t\t" + wt[i]);
-            }
+            }                 
             float atat = (float) totalTime / n;
-            float awt = (float) totalTime / n;
-            System.out.println("Average TAT : " + atat);
-            System.out.println("\nAverage WT : " + awt + "\n");
+            float awt = (float) calculateAverage(wt, n);
+            System.out.println("\nAverage TAT : " + atat);
+            System.out.println("Average WT : " + awt + "\n");
         }
+    }
+    private static float calculateAverage(int[] arr, int n) {
+        float sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        return sum / n;
     }
 }
